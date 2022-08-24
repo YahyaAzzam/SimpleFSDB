0c08a0d266c2c6f0941a285fdbca08014da36e1e
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-08-24 20:26:13 +0200

    Revert "Merge branch 'y'"
    This reverts commit 7e001cedaadd759866a0518ea9ad4cbb30e95437, reversing
    changes made to 3229c109fd118acfe0038b99ef68a62f303fd6f5.

--- DB.py
+++ DB.py
@@ -19,7 +19,7 @@ def parse_args():
     )
     parser.add_argument(
         "-db",
-        "--database",
+        "--data-base",
         type=str,
         help="The database where the data wanted is stored",
     )
@@ -36,76 +36,24 @@ def parse_args():
         help="Primary key is a unique key",
     )
     parser.add_argument(
-        "-p",
-        "--parameter",
-        type=str,
-        help="Parameter desired to be set",
-    )
-    parser.add_argument(
         "-v",
         "--value",
         type=str,
-        help="Value of the parameter",
+        help="Value desired to be set",
     )
     return parser.parse_args()
 
 
-def search(path, primary_key):
-    y = False
-    for f in os.walk(path):
-        if primary_key in f:
-            y = True
-            break
-    return y
-
-
-def creates(schema):
-    x = json.load(open(schema, 'r'))
-    p = os.path.join(os.getcwd(), x['database_name'])
-    os.mkdir(p)
-    for i in x['Tables']:
-        d = os.path.join(p, i['name'])
-        os.mkdir(d)
-
-
-def sets(database, table, primary_key, parameter, value):
-    x = gets(database, table, primary_key)
-    if x == "Error":
-        return x
-    else:
-        x[parameter] = value
-        return True
-
-
-def gets(database, table, primary_key):
-    path = os.getcwd() + '\\' + database + '\\' + table
-    if search(path, primary_key):
-        return json.load(open(path + '\\' + primary_key, 'r'))
-    else:
-        return "Error"
-
-
-def deletes(database, table, primary_key):
-    path = os.getcwd() + '\\' + database + '\\' + table
-    if search(path, primary_key):
-        os.remove(path + '\\' + primary_key)
-        return True
-    else:
-        return "Error"
+def create(schema):
+    print(schema)
 
 
 args = parse_args()
 if args.command == "create":
-    creates(args.schema)
-elif args.command == "set":
-    m = sets(args.database, args.table, args.primary_key, args.parameter, args.value)
-    if "Error" == m:
-        print(m)
-elif args.command == "get":
-    m = gets(args.database, args.table, args.primary_key)
-    if "Error" == m:
-        print(m)
-elif args.command == "delete":
-    m = deletes(args.database, args.table, args.primary_key)
-    if "Error" == m:
-        print(m)
+    create(args.schema)
+# elif args.command == "set":
+#    set(args.database,args.table,args.primary_key,args.value)
+# elif args.command == "get":
+#    get(args.database,args.table,args.primary_key)
+# elif args.command == "delete":
+#    delete(args.database,args.table,args.primary_key)
