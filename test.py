5f2280e2d61ce36d3996fdac63424962805bf00b
Mahmoud goda <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-08-29 23:27:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,31 +6,31 @@ import unittest
 class Test(unittest.TestCase):
 #create the main dir and fix it if there is any problem
     os.system("main.py -c create_dir -sc Check-in-schema.json")
-    def test_1(self):
+    def test_get(self):
         #create json file in seats table then get data from it
         os.system("main.py -c create -sc Check-in-schema.json -t Seats -pk 1.json")
         x=gets("csed25","Seats","1.json")
         ans={'Last_name': '0', 'ReservationId': '0'}
         self.assertEqual(x,ans)
-    def test_2(self):
+    def test_get_nothing(self):
         #get data from json file but i didn't create this file
         x=gets("csed25","Seats","2.json")
         self.assertEqual(x,False)# there is no json file so it return False
-    def test_3(self):
+    def test_set(self):
         #set the last name in the json file to(goda) and the id to(123)
         sets("csed25","Seats","1.json","Last_name","goda")
         sets("csed25","Seats","1.json","ReservationId","123")
         x=gets("csed25","Seats","1.json")
         ans={'Last_name': 'goda', 'ReservationId': '123'}
         self.assertEqual(x,ans)
-    def test_4(self):
-        #set the value in uncreated file to(goda) and(123) this will create the file with these values
+    def test_set_NewFile(self):
+        #set the value in uncreated file to(goda) and(123) this will create the file with the given values
         sets("csed25","Seats","2.json","Last_name","goda")
         sets("csed25","Seats","2.json","ReservationId","123")
         x=gets("csed25","Seats","2.json")
         ans={'Last_name': 'goda', 'ReservationId': '123'}
         self.assertEqual(x,ans)
-    def test_5(self):
+    def test_deleteFiles(self):
         #delete all the created files
         deletes("csed25","Seats","1.json")
         deletes("csed25","Seats","2.json")
