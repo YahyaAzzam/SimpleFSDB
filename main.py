12b3bb597e4431fdf740cd6d6a342724c9b2b8ed
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-03 00:41:39 +0200

    testing workspace

--- main.py
+++ main.py
@@ -2,6 +2,4 @@ from Command.input import *
 from Command.command_factory import *
 
 command = CommandFactory.create(parse_args())
-if command is not None:
-    output_message = command.execute()
-    print(output_message)
+output_message = command.execute()
