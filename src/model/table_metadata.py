62b8a9fb0d6b664482601c909a191aef6576134c
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-12 14:24:10 +0200

    Update table_metadata.py

--- src/model/table_metadata.py
+++ src/model/table_metadata.py
@@ -5,27 +5,24 @@ from model.index import *
 
 class TableMetaData:
     def __init__(self, table_map, path):
-        self.name = table_map[Keys.NAME]
-        self.primary_key = table_map[Keys.PRIMARY_KEY]
-        self.columns = table_map[Keys.COLUMNS]
-        self.indices = table_map[Keys.INDEX_KEYS]
-        self.path = os.path.join(path, self.name)
+        self.table_map = table_map
+        self.path = os.path.join(path, self.table_map[Keys.NAME])
         self.__validate__(self)
-        self.serialize(table_map)
+        self.serialize()
 
-    def serialize(self, table_map):
+    def serialize(self):
         os.makedirs(self.path, exist_ok=True)
-        with open(os.path.join(self.path, "{}_schema.json".format(self.name)), 'w') as file:
-            json.dump(table_map, file)
-        self.__create_indices__(self, table_map)
+        with open(os.path.join(self.path, "{}_schema.json".format(self.table_map[Keys.NAME])), 'w') as file:
+            json.dump(self.table_map, file)
+        self.__create_indices__(self)
 
     @staticmethod
-    def __create_indices__(self, table_map):
-        for index in self.indices:
-            index = Index(index, table_map, self.path)
+    def __create_indices__(self):
+        for index in self.table_map[Keys.INDEX_KEYS]:
+            index = Index(index, self.table_map, self.path)
             index.create()
 
     @staticmethod
     def __validate__(self):
-        if self.primary_key is None or self.primary_key not in self.columns:
+        if self.table_map[Keys.PRIMARY_KEY] is None or self.table_map[Keys.PRIMARY_KEY] not in self.table_map[Keys.COLUMNS]:
             raise WrongParameterError("Primary_key not found")
