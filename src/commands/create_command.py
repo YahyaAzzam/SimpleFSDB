7b55788f19912289f6b2a3280e4ea1d4a8663341
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-14 13:58:00 +0200

    Update create_command.py

--- src/commands/create_command.py
+++ src/commands/create_command.py
@@ -7,17 +7,13 @@ from model.table_metadata import *
 class CreateCommand(AbstractCommand):
     def __init__(self, schema_file):
         self.validate(schema_file, Keys.SCHEMA_PATH)
-        schema_file = os.path.join(Keys.SCHEMA_PATH, schema_file)
-        with open(schema_file, 'r') as file:
-            self.data = json.load(file)
-        self.check_database()
+        self.__get_database_schema__()
+        self.__check_database__()
         self.path = os.path.join(Keys.DATABASE_PATH, self.data[Keys.DATABASE])
 
     def execute(self):
-        os.makedirs(self.path, exist_ok=True)
-        for table in self.data[Keys.TABLES]:
-            table_metadata = TableMetaData(table, self.path, self.data[Keys.NAME])
-            table_metadata.serialize()
+        self.__create_database__()
+        self.__create_tables__()
 
     @staticmethod
     def validate(schema_file, schema_dir):
@@ -27,6 +23,20 @@ class CreateCommand(AbstractCommand):
         if not os.path.exists(path):
             raise WrongParameterError("Schema doesn't exist")
 
-    def check_database(self):
+    def __check_database__(self):
         if self.data[Keys.DATABASE] is None:
             raise WrongParameterError("No database detected")
+
+    def __get_database_schema__(self):
+        schema_file = os.path.join(Keys.SCHEMA_PATH, schema_file)
+        with open(schema_file, 'r') as file:
+            self.data = json.load(file)
+
+    def __create_database__(self):
+        os.makedirs(self.path, exist_ok=True)
+
+    def __create_tables__(self):
+        for table in self.data[Keys.TABLES]:
+            table_metadata = TableMetaData(table, self.path, self.data[Keys.DATABASE])
+            table_metadata.serialize()
+
