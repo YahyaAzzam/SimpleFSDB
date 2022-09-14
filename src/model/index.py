3af90f05996e910a528410c46840ae481b4dfb95
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-09-15 00:00:49 +0200

    fix

--- src/model/index.py
+++ src/model/index.py
@@ -1,4 +1,4 @@
-import os
+import os, pathlib
 from output.exceptions import *
 
 
@@ -17,3 +17,30 @@ class Index:
 
     def create(self):
         os.makedirs(self.path, exist_ok=True)
+
+    def get_primary_keys(self, value_name):
+        with open(os.path.join(self.path, "{}.json".format(value_name)), 'r') as file:
+          return json.load(file)
+
+    def update_value(self, value_name, value):
+       with open(os.path.join(self.path, "{}.json".format(value_name)), 'w') as file:
+           json.dump(value)
+
+    def add_value(self, value_name, primary_key):
+        path = os.path.join(self.path, value_name)
+        if os.path.exists(path):
+            value = self.get_primary_keys(value_name)
+            value.append(primary_key)
+            update_value(value_name, value)
+        else:
+            update_value(value_name, {primary_key})
+
+    def remove_value(self, value_name, primary_key):
+        path = os.path.join(self.path, value_name)
+        if os.path.exists(path):
+            value = self.get_primary_keys(value_name)
+            value.discard(primary_key)
+            if(value == {}):
+                pathlib.Path(path).unlink()
+            else:
+                update_value(value_name, value)
