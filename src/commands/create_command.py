e6dad3643ec9b6915af4ac67af4ae56442b69496
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-14 14:00:36 +0200

    Update create_command.py

--- src/commands/create_command.py
+++ src/commands/create_command.py
@@ -5,9 +5,9 @@ from model.table_metadata import *
 
 
 class CreateCommand(AbstractCommand):
-    def __init__(self, schema_file):
-        self.validate(schema_file, Keys.SCHEMA_PATH)
-        self.__get_database_schema__()
+    def __init__(self, schema_name):
+        self.validate(schema_name, Keys.SCHEMA_PATH)
+        self.__get_database_schema__(schema_name)
         self.__check_database__()
         self.path = os.path.join(Keys.DATABASE_PATH, self.data[Keys.DATABASE])
 
@@ -16,10 +16,10 @@ class CreateCommand(AbstractCommand):
         self.__create_tables__()
 
     @staticmethod
-    def validate(schema_file, schema_dir):
-        if schema_file is None or schema_file == "" or schema_file == " ":
+    def validate(schema_name, schema_dir):
+        if schema_name is None or schema_name == "" or schema_name == " ":
             raise NoParameterError("Schema parameter not entered")
-        path = os.path.join(schema_dir, str(schema_file))
+        path = os.path.join(schema_dir, str(schema_name))
         if not os.path.exists(path):
             raise WrongParameterError("Schema doesn't exist")
 
@@ -27,8 +27,8 @@ class CreateCommand(AbstractCommand):
         if self.data[Keys.DATABASE] is None:
             raise WrongParameterError("No database detected")
 
-    def __get_database_schema__(self):
-        schema_file = os.path.join(Keys.SCHEMA_PATH, schema_file)
+    def __get_database_schema__(self, schema_name):
+        schema_file = os.path.join(Keys.SCHEMA_PATH, schema_name)
         with open(schema_file, 'r') as file:
             self.data = json.load(file)
 
