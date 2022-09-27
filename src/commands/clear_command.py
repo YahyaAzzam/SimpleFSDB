24c1021b08b54248348f9da0da39cd5c90c761a0
Mahmoud goda <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-09-27 02:36:32 +0200

    ClearCommand (#7)
    Clear command implemented

---
+++ src/commands/clear_command.py
@@ -0,0 +1,17 @@
+from commands.abstract_command import *
+from model.database import *
+
+
+class ClearCommand(AbstractCommand):
+    def __init__(self, database_name):
+        ClearCommand.validate(database_name)
+        self.database_name = database_name
+
+    def execute(self):
+        database = Database(database_name = self.database_name)
+        database.clear()
+
+    @staticmethod
+    def validate(database_name):
+        if database_name == None or len(database_name) == 0:
+            raise NoParameterError("database_name parameter not entered")
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ from test_create import *
 from test_set import *
 from test_delete import *
 from test_get import *
+from test_clear import *
 
 
 DATABASE_PATH = os.path.join(str(os.getcwd()).replace("commands", '').replace("src", '').replace("tests", ''), 'storage')
