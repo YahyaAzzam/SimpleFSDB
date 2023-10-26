3225ef95030af628666777cc126fca7fbaa0ee60
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2023-10-26 19:46:25 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,6 @@ from test_delete import *
 from test_get import *
 from test_clear import *
 
-
-DATABASE_PATH = os.path.join(os.path.dirname(os.getcwd()), 'Querio', 'storage')
+DATABASE_PATH = os.path.join(str(os.getcwd()).replace("commands", '').replace("Querio", '').replace("tests", ''), 'storage')
 if os.path.exists(DATABASE_PATH):
     shutil.rmtree(DATABASE_PATH)
