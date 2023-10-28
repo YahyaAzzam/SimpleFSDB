10aac6a936d357664440bea008a6b40878115a73
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-10-29 00:27:44 +0200

    fixing the workflow

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ author_email_1 = 'mahmoden17@gmail.com'
 # Combine author information
 authors = f"{author_name_1} <{author_email_1}>, {author_name_2} <{author_email_2}>"
 
+
 # Setting up
 setup(
     name="Querio",
